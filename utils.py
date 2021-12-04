2742d15ed854446762276c09f8cd474a4547e4e0
ronenius <ronenius4@gmail.com>
2021-12-04 18:27:20 +0200

    added special send and receive function for files

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-import sys
+import socket
+import os
 import time
 import logging
 from watchdog.observers import Observer
@@ -31,6 +32,91 @@ def get_observer(path):
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     return observer
+
+
+def send_path(path, sock):
+    sock.send(path.encode('utf-8'))
+
+
+def receive_path(sock):
+    return sock.recv(1024).decode('utf-8')
+
+
+def send_file(path, sock):
+    send_path(path, sock)
+    f = open(path, 'r')
+    l = f.read(1)
+    while l:
+        sock.send(l.encode('utf-8'))
+        l = f.read(1)
+    sock.send("end".encode('utf-8'))
+    f.close()
+
+
+def receive_file(sock):
+    path = receive_path(sock)
+    if path == "finish" or path == "end files":
+        return -1
+    f = open(path, 'w')
+    l = sock.recv(1).decode('utf-8')
+    while l != "end":
+        f.write(l)
+        l = sock.recv(1)
+    f.close()
+    return 0
+
+
+def remove_file(path):
+    if os.path.exists(path):
+        os.remove(path)
+
+
+def remove_directory(path):
+    for root, dirs, files in os.walk(path, topdown=False):
+        for f in files:
+            os.unlink(os.path.join(root, f))
+        for d in dirs:
+            os.rmdir(os.path.join(root, d))
+    os.rmdir(path)
+
+
+def send_empty_subdirs(path, sock):
+    for root, dirs, files in os.walk(path):
+        for d in dirs:
+            send_path(os.path.join(root, d), sock)
+    sock.send("end sub dirs".encode('utf-8'))
+
+
+def send_files(path, sock):
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            send_file(os.path.join(root, f), sock)
+    sock.send("end files".encode('utf-8'))
+
+
+def send_dir(path, sock):
+    send_path(path, sock)
+    send_empty_subdirs(path, sock)
+    send_files(path, sock)
+
+
+def receive_empty_subdirs(sock):
+    path = receive_path(sock)
+    while path != "end sub dirs":
+        os.mkdir(path)
+        path = receive_path(sock)
+
+
+def receive_files(sock):
+    while receive_file(sock) != -1:
+        pass
+
+
+def receive_dir(sock):
+    path = receive_path(sock)
+    os.mkdir(path)
+    receive_empty_subdirs(sock)
+    receive_files(sock)
 
 
 if __name__ == "__main__":
